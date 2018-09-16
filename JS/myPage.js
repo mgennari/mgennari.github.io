bf22d7a6bdadaa88420b4d5f4b076acdac28ab93
mgennari <mgennari5216@gmail.com>
2018-09-15 21:52:19 -0400

    - large updates have been made to the website - overall format has been improved - removed the original research section and replaced with new research section (old publications section) - should by much more clean now - just have to update the travel section description and astronomy - new photos look better I think

--- JS/myPage.js
+++ JS/myPage.js
@@ -6,7 +6,7 @@
     //Controllers in order of appearance
     myWebpage.controller("allTabs", function () {
         this.tab = 1;
-        this.numTabs = 4;
+        this.numTabs = 3;
         this.tabSet = function (checkTab) {
             return this.tab === checkTab;
         };
@@ -33,22 +33,26 @@
 
     myWebpage.controller("mySummary", function () {
         this.summary = [
-            ["Advanced background in nuclear theory from research in TRIUMF's Theory Group"],
-            ["Branching knowledge in machine learning and applications to the nuclear many-body problem"],
-            ["Proficient in several programming languages such as FORTRAN, Python, R"],
-            ["Experienced in communicating advanced content in mathematics and physics to varying audiences"]
+            ["Extensive background in mathematics and physics through degree requirments"],
+            ["Advanced knowledge in nuclear physics from work on " + "ab initio" + " nuclear theory at TRIUMF"],
+            ["First authored two manuscripts from work at TRIUMF (see research tab)"],
+            ["Studying machine learning and applications to the nuclear many-body problem"],
+            ["Experienced with several programming languages, in addition to OpenMP and MPI parallelization tecnhiques"],
+            ["Proficiency as an educator achieved through direct tutoring and volunteering positions"],
         ]
     });
 
     myWebpage.controller("photosController", function () {
         this.images = [
-            ["images/japan-garden.jpg","Japanese Garden, DREB 2018 Conference."],
-            ["images/dnp-pittsburgh.jpg","APS DNP in Pittsburgh, USA."],
+            ["images/japan-garden.jpg","Japanese garden at DREB 2018 conference in Matsue, Japan."],
+            ["images/cupc-edmonton.jpg","Poster discussion at CUPC 2018 conference in Edmonton, Canada."],
+            ["images/dsqc-conference.jpeg","Data Science and Quantum Computing workshop at TRIUMF, Canada."],
+            ["images/dnp-pittsburgh2.jpg", "Poster presentation during APS DNP in Pittsburgh, U.S.A."],
+            ["images/san-marino-church.jpg", "Church in the Republic of San Marino."],
             ["images/roman-ruins.jpg", "Roman ruins in Palmera, Syria."],
-            ["images/syria-storm.jpg", "Sandstorm sunset near Aleppo, Syria."],
-            ["images/san-marino-church.jpg", "The Republic of San Marino."],
-            ["images/algonquin-park.jpeg", "Algonquin Park, Canada."],
-            ["images/whistler-ziplining.jpg", "Whistler Mountain, British Columbia."],
+            ["images/syria-storm.jpg", "Sandstorm sunset in Aleppo, Syria."],
+            ["images/algonquin-park.jpeg", "Canoeing in Algonquin Park, Canada."],
+            ["images/whistler-ziplining.jpg", "Ziplining in Whistler Mountain, British Columbia."],
         ];
         this.currPhoto = 0;
         this.incPhoto = function () {
@@ -67,37 +71,28 @@
         }
     });
 
-    myWebpage.controller("researchController", function () {
-        this.nlocdens = [
-            ["images/research/He4_nloc_ndens_gscompNNn4lo5003Nloc650nonloc500.png", "There have been substantial advancements in ab initio techniques in nuclear theory, allowing us to extract crucial quantities from our many-body wavefunctions. Nonlocal nuclear densities can provide insight into nuclear structure, act as input for optical potentials at high energy, and are a central object of density functional theory."],
-            ["images/research/4He_200MeV_local_nonlocal.png", "A nonlocal translationally invariant (trinv) nuclear density is required for realistic calculations. The act of centre of mass (wiCOM) removal generates a more physically accurate density, and a nonlocal form of the density operator (instead of local density) is a more fundamental object to work with. Here we show differences between the COM contaminated density and the translationally invariant density for light nuclei in generating differential cross sections. One can clearly see the significance of COM removal in calculations involving light nuclei. These calculations can be performed for more exotic systems, such as the halo nuclei He-6,8. The recent prediction of experimental results for He-6 at 200 MeV has further credited the accuracy of the model."],
-        ];
-        this.kdens = [
-            ["images/research/O16_nkdens_gscompNNn4lo5003Nloc650nonloc500.png", "Nuclear densities are a fundamental object of density functional theory (DFT), the objective of which is to minimize an energy functional written in terms of varying densities (charge, spin, isospin, etc.). The COM contamination effects are reduced in larger A-nucleon systems, however if DFT is to extend it's reach to lighter systems the contamination in the theory must be systematically studied. The derivation of the kinetic density (shown to the left) from ab initio wavefunctions allows us to begin to gauge the importance of translational invariance in the energy functionals."],
-        ];
-        this.natorbs = [
-            [],
-        ];
-    });
-
     myWebpage.controller("publicationsController", function () {
-        this.researchSection = "Published Papers";
+        this.researchSection = "Publications";
         this.researchPapers = [
-            ["https://arxiv.org/abs/1712.02879", "M. Gennari, M. Vorabbi, A. Calci, and P. Navrátil, Phys. Rev. C 97, 034619 (2018)"]
+            ["https://arxiv.org/abs/1712.02879", "M. Gennari, M. Vorabbi, A. Calci, and P. Navrátil. Phys. Rev. C 97, 034619 (2018)"],
+            ["https://arxiv.org/abs/1808.10537", "M. Gennari and P. Navrátil. arXiv:1808.10537 (2018)"]
         ];
-        this.prespostSection = "Presentations and Posters";
+        this.prespostSection = "Presentations and posters";
         this.prespostList = [
-            ["research/presentations/dreb2018-poster.pdf", "Direct Reactions with Exotic Beams 2018 Poster, TRIUMF"],
-            ["research/presentations/ab-initio-wrkshp-pres-W18.pdf", "Progress in Ab Initio Techniques in Nuclear Physics 2018 Presentation, TRIUMF"],
-            ["research/presentations/ab-initio-wrkshp-poster-W18.pdf", "Progress in Ab Initio Techniques in Nuclear Physics 2018 Poster, TRIUMF"],
-            ["research/presentations/DNP_CEU_poster-F18.pdf", "APS Division of Nuclear Physics Meeting 2017 Poster, Pittsburgh, USA."],
-            ["research/presentations/three_min_thesis.pdf", "Three Minute Thesis Competition 2017 Presentation at TRIUMF"],
-            ["research/presentations/nldens-acot-pres-w17.pdf", "Advisory Committee on TRIUMF Winter 2017 Presentation, Theory Group"],
+            ["research/presentations/CUPC-S18-pres.pdf", "Canadian Undergraduate Physics Conference 2018 Presentation - University of Alberta, Canada."],
+            ["research/presentations/CUPC-S18-poster.pdf", "Canadian Undergraduate Physics Conference 2018 Poster - University of Alberta, Canada."],
+            ["research/presentations/dreb2018-poster.pdf", "Direct Reactions with Exotic Beams 2018 Poster - Matsue, Japan."],
+            ["research/presentations/ab-initio-wrkshp-pres-W18.pdf", "Progress in Ab Initio Techniques in Nuclear Physics 2018 Presentation - TRIUMF, Canada."],
+            ["research/presentations/ab-initio-wrkshp-poster-W18.pdf", "Progress in Ab Initio Techniques in Nuclear Physics 2018 Poster - TRIUMF, Canada."],
+            ["research/presentations/DNP_CEU_poster-F18.pdf", "APS Division of Nuclear Physics Meeting 2017 Poster - Pittsburgh, U.S.A."],
+            ["research/presentations/three_min_thesis.pdf", "Three Minute Thesis Competition 2017 Presentation - TRIUMF, Canada"],
+            ["research/presentations/nldens-acot-pres-w17.pdf", "Advisory Committee on TRIUMF Theory Group 2017 Presentation - TRIUMF, Canada"],
         ];
-        this.reportSection = "Work Reports";
+        this.reportSection = "Work reports";
         this.workReports = [
-            ["research/work_reports/Analysis2B3Bchiral.pdf", "Gennari, M. (2018). Analysis of recent two- and three-body nuclear interactions. TRIUMF, Theory Group. UBC, Vancouver, British Columbia."],
-            ["research/work_reports/NonlocalTRINVDensity-MKI.pdf", "Gennari, M. (2017). Nonlocal Translationally Invariant Nuclear Density. TRIUMF, Theory Group. UBC, Vancouver, British Columbia."],
+            ["research/work_reports/kdens.pdf", "Gennari, M. (2018). Kinetic density from ab initio theory. TRIUMF, Theory Group. UBC, Vancouver, British Columbia."],
+            ["research/work_reports/chiralEFT_2B3Bint.pdf", "Gennari, M. (2017). Analysis of recent two- and three-body nuclear interactions. TRIUMF, Theory Group. UBC, Vancouver, British Columbia."],
+            ["research/work_reports/trinv_nlocdens.pdf", "Gennari, M. (2017). Nonlocal Translationally Invariant Nuclear Density. TRIUMF, Theory Group. UBC, Vancouver, British Columbia."],
             ["research/work_reports/MathCentreWR.pdf", "Gennari, M. (2016). Evaluating the Effectiveness of Math Centre Advertising Methods. Humber College Math Centre. Humber College, Toronto, Ontario."],
             ["research/work_reports/MathCentreSIRF.pdf", "Gennari, M. (2016). Humber Math Centre SIRF Summary Report. Humber College Math Centre. Humber College, Toronto, Ontario."]
         ];
@@ -105,8 +100,8 @@
 
     myWebpage.controller("resumeController", function () {
         this.myResumes = [
+            ["resumes/cv-mgennari-Sep2018.pdf", "Michael Gennari's Curriculum Vitae (Updated September 2018)"],
             ["resumes/General Resume April 2018.pdf", "Michael Gennari's Resume (Updated June 2018)"],
-            ["resumes/General CV April 2018.pdf", "Michael Gennari's Curriculum Vitae (Updated June 2018)"],
         ];
     });
 
